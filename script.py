f2a305c04a6c99a688c74bdc85f532b4a5453da5
danpucjim <dpuchejimenez@gmail.com>
2022-09-20 16:45:38 +0200

    .

--- script.py
+++ script.py
@@ -14,24 +14,9 @@ script.py sha1"""
 
 hashes = dict() #directorio con el hash calculado la primera vez
 new_hash = dict() # directorio con el hash calculado ahora mismo para compararlo con el antiguo
+CONTADOR = 0
 
 #FUNCIONES
-
-def main():
-    args = sys.argv # guardamos los argumentos que pasamos en el script y comprobamos que algoritmo utilizara
-
-    if(len(args)>1):
-        #si ejecutamos el script.py sha256
-        if(args[1] == 'sha256'):
-            all_directories('sha256','.',hashes)
-            comp_hash('sha256','.')
-            return args
-        # si ejecutamos el script.py sha1
-        elif(args[1] == 'sha1'):
-            all_directories('sha1','.',hashes)
-            return args
-
-    return TEXTO
     
 #algoritmo de hash para sha256
 def alg_sha256(file):
@@ -60,11 +45,9 @@ def all_directories(alg,directorio,dict):
     for directorio in directorios:
         #ESTO TENGO QUE CAMBIARLO!!
         if(directorio=='.git'):
-            print("NO ACCESO")
+            pass
         else:
             dict[directorio] = alg_sha256(directorio)
-            guardar_hash("hash",dict)
-    print(hashes)
 
 # guarda el directorio : hash en el archivo hash para compararlo cada dia al recalcular los hashes
 def guardar_hash(file,dict):
@@ -74,10 +57,46 @@ def guardar_hash(file,dict):
         pfile.write("\n")
 
 #Comparador de hashes(hay que terminarlo)
-
 def comp_hash(alg,directorio):
+    contador = 0
     all_directories(alg,directorio,new_hash)
-    print("ESTES",new_hash)
+    for keys,values in hashes.items():
+        for keys1,values1 in new_hash.items():
+            print(values1)
+            if values1 == values:
+                contador
+                break
+            else:
+                contador+=1
+                print(values1,values)
+                break
+    print(contador)
+    return contador
+
+#guardamos los datos del archivo hash en la variable hashes
+def recuperar_hash():
+    file = open("hash","r")
+    print(file)
+    for linea in file:
+        datos = linea.split(":")
+        hashes[datos[0]] = datos[1].split('\n')[0]
+
+
+def main():
+    args = sys.argv # guardamos los argumentos que pasamos en el script y comprobamos que algoritmo utilizara
+    recuperar_hash()
+    if(len(args)>1):
+        #si ejecutamos el script.py sha256
+        if(args[1] == 'sha256'):
+            all_directories('sha256','.',hashes)
+            comp_hash('sha256','.')
+            return args
+        # si ejecutamos el script.py sha1
+        elif(args[1] == 'sha1'):
+            all_directories('sha1','.',hashes)
+            return args
+
+    return TEXTO
 
 #-----------------------------------------------------------------------------------------------------#
 main()
