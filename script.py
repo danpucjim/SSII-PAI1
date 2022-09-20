2b01580b69a426de3c36bec77a7bb2962cb0b44b
danpucjim <dpuchejimenez@gmail.com>
2022-09-20 11:24:22 +0200

    pequenyo cambio

--- script.py
+++ script.py
@@ -2,39 +2,40 @@ import base64
 import hashlib
 import sys
 
-
-
 # CONSTANTES GLOBALES
 BLOCK_SIZE = 65536 # tamanyo de cada bloque del archivo
 FILE = "./README.md"
 TEXTO = """Este script comprobara diariamente que los datos siguen siendo legitimos, usted podra hacer uso de este con el algoritmo sha256 y el algoritmo sha1 de la siguiente manera
- script.py sha256
- script.py sha1"""
+script.py sha256
+script.py sha1"""
 
+#FUNCIONES
 
 def main():
     args = sys.argv # guardamos los argumentos que pasamos en el script y comprobamos que algoritmo utilizara
-    print(args)
-    if(args[1]=='sha256'):
-        alg_sha256()
-        return args
-    elif(args[1]=='sha1'):
-        alg_sha1()
-        return args
-    else:
-        return TEXTO
+    
+    if(len(args)>1):
+        #si ejecutamos el script.py sha256
+        if(args[1] == 'sha256'):
+            print(alg_sha256())
+            return args
+        # si ejecutamos el script.py sha1
+        elif(args[1] == 'sha1'):
+            print(alg_sha1())
+            return args
 
+    return TEXTO
+    
 #algoritmo de hash para sha256
 def alg_sha256():
     h = hashlib.sha256()
-    lect_arch(FILE,BLOCK_SIZE,h)
-    return 0
+    return lect_arch(FILE,BLOCK_SIZE,h)
 
 #algoritmo de hash para sha1
 def alg_sha1():
     h = hashlib.sha1()
-    lect_arch(FILE,BLOCK_SIZE,h)
-    return 0
+    return lect_arch(FILE,BLOCK_SIZE,h)
+    
 
 # lee el archivo que queremos hashear
 def lect_arch(archivo,bs,h):
@@ -43,6 +44,8 @@ def lect_arch(archivo,bs,h):
         while len(fb) > 0:
             h.update(fb)
             fb = f.read(bs)
-    print(h.hexdigest())
+    return h.hexdigest()
 
-print(main())
+
+#-----------------------------------------------------------------------------------------------------#
+main()
